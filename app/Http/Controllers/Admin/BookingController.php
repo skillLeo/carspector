3849fdce04c5f2caf623eb5cd868a78dca63765e
saif-dev154 <saif.dev154@gmail.com>
2026-05-05 05:54:37 +0500

    Sort bookings by date and time newest first

--- app/Http/Controllers/Admin/BookingController.php
+++ app/Http/Controllers/Admin/BookingController.php
@@ -344,7 +344,7 @@ class BookingController extends Controller
         $bookings = Order::query()
             ->select('orders.*')
             ->with(['examiner','user','b2bPartner'])
-            ->orderByRaw('COALESCE(orders.admin_order_date, DATE(orders.created_at)) DESC');
+            ->orderByRaw('COALESCE(orders.admin_order_date, DATE(orders.created_at)) DESC, orders.created_at DESC');
 //        dd($bookings);
 //        dd($bookings);
         $bookings = $bookings->when($request->date_range != '', function ($q) {
